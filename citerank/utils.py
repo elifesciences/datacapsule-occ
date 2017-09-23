1ad01d3910d82530f4941195844d33d5b9086d8e
Daniel Ecer <de-code@users.noreply.github.com>
2017-09-23 21:43:54 +0100

    create temp csv file

--- citerank/utils.py
+++ citerank/utils.py
@@ -6,6 +6,8 @@ from contextlib import contextmanager
 
 import six
 
+TEMP_FILE_SUFFIX = '.part'
+
 def makedirs(path, exist_ok=False):
   try:
     # Python 3
@@ -100,7 +102,7 @@ def gzip_open(filename, mode):
     return gzip.open(filename, mode)
 
 def optionally_compressed_open(filename, mode):
-  if filename.endswith('.gz'):
+  if filename.endswith('.gz') or filename.endswith('.gz' + TEMP_FILE_SUFFIX):
     return gzip_open(filename, mode)
   else:
     return open(filename, mode)
@@ -123,7 +125,11 @@ def write_csv_row(writer, row):
   write_csv_rows(writer, [row])
 
 def write_csv(filename, columns, iterable):
-  with open_csv_output(filename) as csv_f:
+  temp_filename = filename + TEMP_FILE_SUFFIX
+  if os.path.isfile(filename):
+    os.remove(filename)
+  with open_csv_output(temp_filename) as csv_f:
     writer = csv.writer(csv_f)
     write_csv_rows(writer, [columns])
     write_csv_rows(writer, iterable)
+  os.rename(temp_filename, filename)
